ed835789c2481707c2836d8510f2ee0e7251fe84
kalpesh <kalpesh162@gmail.com>
2026-01-04 09:48:42 +0530

    second

--- student-app/student-app/src/main/java/com/exampple/student_app/security/JwtUtil.java
+++ student-app/student-app/src/main/java/com/exampple/student_app/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.exampple.student_app.security;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -11,7 +12,8 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtUtil {
 
-	private final String SECRET = "mySecretKeyMySecretKeyMySecretKey";
+	@Value("${jwt.secret}")
+	private String SECRET;
 
 	public String generateToken(String username) {
 		return Jwts.builder().setSubject(username).setIssuedAt(new Date())
